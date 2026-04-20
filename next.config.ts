56024edf18fd34488b1131e9ad0e64a10e7c55ef
Ryosuke Yoshioka <ryosuke2170@gmail.com>
2026-04-21 01:06:47 +0900

    Fix GitHub Pages basePath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/developer-portfolio',
+  assetPrefix: '/developer-portfolio',
   images: {
     unoptimized: true,
   },
